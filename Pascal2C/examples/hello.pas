ca1c1e97265edb7d111418980857d31000b0a0e8
Word2VecT <tangzinan@bupt.edu.cn>
2025-04-14 17:04:20 +0800

    📝 Simple hello.pas

--- Pascal2C/examples/hello.pas
+++ Pascal2C/examples/hello.pas
@@ -6,24 +6,4 @@ var
 
 begin
     greeting := 'Hello, World!';
-    writeln(greeting);
-    
-    sum := 0;
-    isRunning := true;
-    
-    i := 1;
-    while i <= 10 do
-    begin
-        sum := sum + i;
-        i := i + 1;
-    end;
-    
-    writeln('Sum of numbers from 1 to 10: ', sum);
-    
-    if sum > 50 then
-        writeln('Sum is greater than 50')
-    else
-        writeln('Sum is less than or equal to 50');
-    
-    isRunning := false;
 end. 
